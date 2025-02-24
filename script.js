25b66745fcb0bbae6a012a1e94a913271bd929c6
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-24 11:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const obj={};
+
+	for(const s of str){
+		obj[s]: (obj[s] || 0)+1;
+	}
+
+	for(const s of obj){
+		if(obj[s]===1){
+			return s;
+		}
+	}
+	return null;
+	
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
